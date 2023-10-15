4fade0b2c708fb486f6fa7822eb2e4f9a0758747
Stefan Netterfield <stefan@5f03.com>
2023-10-15 00:12:54 -0400

    Implemented unit test for read/write functions for city population JSON file...

---
+++ tests/data.js
@@ -0,0 +1,37 @@
+import { deepEqual } from 'assert';
+import { writeFile, readFile, rm } from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+import { join, resolve } from 'node:path';
+import { readPopulationFile, writePopulationFile } from '../lib/population_file.js';
+import populationData from '../data/city_population_obj.js';
+
+describe('Reading and Writing Population data to JSON file', () => {
+    const relativateDataPath = resolve('./data');
+    const testFileNameRead = 'test_population_read.json';
+    const testFileNameWrite = 'test_population_write.json';
+    const testPathRead = join(relativateDataPath, testFileNameRead);
+    const testPathWrite = join(relativateDataPath, testFileNameWrite);
+
+    before(async () => {
+        await writeFile(testPathRead, JSON.stringify(populationData), { encoding: 'utf-8' });
+    });
+    
+    it('Reading Population Data', async () => {
+        const populationDataRead = await readPopulationFile(testFileNameRead);
+
+        deepEqual(populationDataRead, populationData);
+    });
+    
+    it('Writing Population Data', async () => {
+        await writePopulationFile(testFileNameWrite, populationData);
+
+        const populationDataWrite = JSON.parse(await readFile(testPathWrite, { encoding: 'utf-8' }));
+
+        deepEqual(populationDataWrite, populationData);
+    });
+
+    after(async () => {
+        if(existsSync(testPathRead)) await rm(testPathRead);
+        if(existsSync(testPathWrite)) await rm(testPathWrite);
+    });
+});
