1242bfcce08ab1aa49e4197f1c837c948bce4356
Stefan Netterfield <stefan@5f03.com>
2023-10-16 03:56:20 -0400

    Implemented logic for PUT method of API endpoint...

--- routes/population.js
+++ routes/population.js
@@ -19,7 +19,8 @@ const paramSchema = {
     additionalProperties: false
 };
 const putBodySchema = {
-    type: 'integer'
+    type: 'integer',
+    nullable: false
 };
 const paramValidator = ajv.compile(paramSchema);
 const bodyValidator = ajv.compile(putBodySchema);
@@ -36,7 +37,7 @@ export default {
             if(err.instancePath === '/city') errMessage = 'City parameter is invalid!';
 
             response.writeHead(400, { 'Content-Type': 'application/json' });
-            response.write(JSON.stringify({error: errMessage}));
+            response.write(JSON.stringify({ error: errMessage }));
             response.end();
             return;
         }
@@ -47,16 +48,83 @@ export default {
 
         if(cities == null || (cities != null && population == null)) {
             response.writeHead(400, { 'Content-Type': 'application/json' });
-            response.write(JSON.stringify({error: 'State or City could not be found!'}));
+            response.write(JSON.stringify({ error: 'State or City could not be found!' }));
             response.end();
             return;
         }
 
         response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({population: population}));
+        response.write(JSON.stringify({ population: population }));
         response.end();
     },
-    put(_, response, params) {
-        response.end();
+    put(request, response, params) {
+        if(request.headers['content-type'] !== 'text/plain') {
+            response.writeHead(400, { 'Content-Type': 'application/json' });
+            response.write(JSON.stringify({ error: 'The body MIME type is invalid!' }));
+            response.end();
+            return;
+        }
+
+        let body = '';
+
+        request.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+
+        request.on('end', () => {
+            const bodyInt = parseInt(body);
+            const paramsValid = paramValidator(params);
+            const bodyValid = bodyValidator(bodyInt);
+
+            if(!paramsValid || !bodyValid) {    
+                let errMessage = '';
+    
+                if(paramValidator.errors != null && paramValidator.errors.length > 0) {
+                    const paramErr = paramValidator.errors[0];
+
+                    if(paramErr.instancePath === '/state') errMessage = 'State parameter is invalid!';
+                    if(paramErr.instancePath === '/city') errMessage = 'City parameter is invalid!';
+
+                } else {
+                    errMessage = 'The population value is invalid!';
+
+                }
+               
+                response.writeHead(400, { 'Content-Type': 'application/json' });
+                response.write(JSON.stringify({ error: errMessage }));
+                response.end();
+                return;
+
+            }
+
+            const { state, city } = params;
+            const cities = this.data.getData(state);
+            const population = cities != null ? cities[city] : null;
+
+            if(cities == null || (cities != null && population == null)) {
+                response.writeHead(400, { 'Content-Type': 'application/json' });
+                response.write(JSON.stringify({ error: 'State or City could not be found!' }));
+                response.end();
+                return;
+            }
+
+            if(bodyInt === population) {
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.write(JSON.stringify({ population: population }));
+                response.end();
+                return;
+            }
+
+            this.data.setData(state, { ...cities, [city]: bodyInt })
+                .then(() => {
+                    response.writeHead(201, { 'Content-Type': 'application/json' });
+                    response.write(JSON.stringify({ population: bodyInt }));
+                    response.end();
+                })
+                .catch((err) => {
+                    response.end();
+                    throw err;
+                });
+        });
     }
 };
